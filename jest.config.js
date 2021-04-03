3fef14e00a42056b3bdcb5ab74c773b45431f72a
gammaSpeck <madhukm3141@gmail.com>
2021-04-03 21:34:46 +0530

    Changed project structure

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['__tests__/setup.ts'],
+  // This moduleWrapper is required to make sure all the tsconfig.paths resolutions work well
   moduleNameMapper: {
     '^@configs': '<rootDir>/src/configs/index.ts',
     '^@libs/(.*)': '<rootDir>/src/libs/$1',
@@ -9,5 +10,6 @@ module.exports = {
     '^@middlewares/(.*)': '<rootDir>/src/middlewares/$1',
     '^@validators/(.*)': '<rootDir>/src/validators/$1'
   },
+  // Setup file for preparing the test environment
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
 }
