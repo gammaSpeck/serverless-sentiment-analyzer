70a49b426d79fd3c1881cda30571cda8abab9dbf
gammaSpeck <madhukm3141@gmail.com>
2021-04-03 20:07:45 +0530

    Added Sentiment analyze code from package sentiment

--- src/routes/sentiment.ts
+++ src/routes/sentiment.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from 'express'
+import Sentiment from 'sentiment'
 
 import { SuccessResponse } from '@libs/success-response'
 import { vSentimentAnalyze } from '@validators/sentiment'
 
 const router = Router()
+const sentiment = new Sentiment()
 
 router.post('/sentiment/analyze', vSentimentAnalyze, async (req: Request, res: Response) => {
-  SuccessResponse.send({ res, data: 'Sentiment analyzed' })
+  const { text } = req.body
+  const data = sentiment.analyze(text)
+
+  SuccessResponse.send({ res, data })
 })
 
 export { router as sentimentRouter }
